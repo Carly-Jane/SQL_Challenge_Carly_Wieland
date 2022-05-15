44006a9bb06850bf7bdf984ec2a7da1dbe31bbcc
Carly-Jane <101085083+Carly-Jane@users.noreply.github.com>
2022-05-15 19:52:07 +0800

    changed table settings

--- SLQ_database/SQL_script.sql
+++ SLQ_database/SQL_script.sql
@@ -1,68 +1,66 @@
+----- Create the database from the following 6 CSV's
+-- departments, titles, employees, salaries, dept_emp, dept_manager
+
+----- Create 'departments' table.
 CREATE TABLE "departments" (
     "dept_no"  varchar  NOT NULL,
     "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+    CONSTRAINT "pk_departments" PRIMARY KEY ("dept_no")
 );
 SELECT *
 FROM departments;
 
+----- Create 'titles' table.
 CREATE TABLE "titles" (
     "title_id"  varchar  NOT NULL,
     "title" varchar   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
+    CONSTRAINT "pk_titles" PRIMARY KEY ("title_id")
 );
 SELECT *
 FROM titles;
 
+----- Create 'employees' table.
 CREATE TABLE "employees" (
-    "emp_no"  SERIAL  NOT NULL,
+    "emp_no"  varchar  NOT NULL,
     "emp_title_id" varchar   NOT NULL,
     "birth_date" varchar	NOT NULL,
     "first_name" varchar   NOT NULL,
     "last_name" varchar   NOT NULL,
     "sex" varchar   NOT NULL,
     "hire_date" varchar   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+    CONSTRAINT "pk_employees" PRIMARY KEY ("emp_no")
 );
 SELECT *
 FROM employees;
 
+----- Create 'salaries' table.
 CREATE TABLE "salaries" (
-    "emp_no"  SERIAL  NOT NULL,
+    "emp_no"  varchar  NOT NULL,
     "salary" int   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+    CONSTRAINT "pk_salaries" PRIMARY KEY ("emp_no")
 );
 SELECT *
 FROM salaries;
 
+----- Create 'dept_emp' table.
 CREATE TABLE "dept_emp" (
     "dept_no" varchar   NOT NULL,
     "emp_no" varchar   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY ("dept_no","emp_no")
 );
 SELECT *
 FROM dept_emp;
 
+----- Create 'dept_manager' table.
 CREATE TABLE "dept_manager" (
     "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
+    "emp_no" varchar   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY ("dept_no","emp_no")
 );
 SELECT *
 FROM dept_manager;
 
+----- Add links as per relational diagram
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
@@ -74,8 +72,8 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
--- ERROR:  foreign key constraint "fk_dept_emp_emp_no" cannot be implemented
--- DETAIL:  Key columns "emp_no" and "emp_no" are of incompatible types: character varying and integer.
+--ERROR:  insert or update on table "dept_emp" violates foreign key constraint "fk_dept_emp_emp_no"
+--DETAIL:  Key (emp_no)=(d005) is not present in table "employees".
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
